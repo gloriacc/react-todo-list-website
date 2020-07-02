5d355e27c16a73ed06362870ce94c473752e1824
gloriacc <xuan_1213@yeah.net>
2020-07-02 16:55:47 +0800

    deploy

---
+++ precache-manifest.00aac0ddf58a702ef1730044470395ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38669a6e057d9f6dd4717fb0ae6d09c",
+    "url": "/react-todo-list-website/index.html"
+  },
+  {
+    "revision": "5cb6839487642eab3aeb",
+    "url": "/react-todo-list-website/static/css/main.c3613089.chunk.css"
+  },
+  {
+    "revision": "f3c51def0bea1a7a9b3f",
+    "url": "/react-todo-list-website/static/js/2.52c9b516.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-list-website/static/js/2.52c9b516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb6839487642eab3aeb",
+    "url": "/react-todo-list-website/static/js/main.a9cbf953.chunk.js"
+  },
+  {
+    "revision": "7001d367c1c83164d17a",
+    "url": "/react-todo-list-website/static/js/runtime-main.f5081c89.js"
+  }
+]);
